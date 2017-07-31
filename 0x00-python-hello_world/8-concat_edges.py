8f204343a45a01edf945980a04a2b983900823e4
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-07-31 21:51:50 +0000

    8-concat_edges.py: simple slicing

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = " ".join([str.split()[5],str.split()[6],str.split()[-4],str.split()[0]])
+str = str[39:67] + str[-22:-17] + str[:6]
 print(str)
