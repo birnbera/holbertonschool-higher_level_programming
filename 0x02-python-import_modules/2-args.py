5d84a1bdc131bd6035f87041037ff5ee805c2a22
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-03 19:32:30 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    nargs = len(sys.argv) - 1
+    print("{:d} {:s}{:s}".format(nargs,
+                                 "argument" if nargs == 1 else "arguments",
+                                 "." if nargs == 0 else ":"))
+    for i, arg in enumerate(sys.argv[1:]):
+          print("{:d}: {:s}".format(i + 1, arg))
