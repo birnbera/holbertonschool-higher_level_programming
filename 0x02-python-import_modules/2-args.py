69a5470e0c83d81b9e05cda2c8639096b807ab41
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-03 23:09:44 +0000

    2-args.py: unfix grammar

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     nargs = len(sys.argv) - 1
     print("{:d} {:s}{:s}".format(nargs,
-                                 "argument" if nargs == 1 else "arguments",
+                                 "argument" if nargs <= 1 else "arguments",
                                  "." if nargs == 0 else ":"))
     for i, arg in enumerate(sys.argv[1:]):
         print("{:d}: {:s}".format(i + 1, arg))
