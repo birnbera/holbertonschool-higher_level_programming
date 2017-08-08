1616c244b5c36289f2e3dec35065f2b45486bdcf
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-08 22:02:45 +0000

    4-new_in_list.py: fix for c style indexing

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 
 def new_in_list(my_list, idx, element):
     new_list = [i for i in my_list]
-    if idx > len(new_list) - 1:
+    if idx < 0 or idx > len(new_list) - 1:
         return new_list
     new_list[idx] = element
     return new_list
