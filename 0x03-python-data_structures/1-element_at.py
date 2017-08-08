d93333df666f075455a08e50ecf3f4dd2992052c
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-08 21:57:04 +0000

    fix for c style indexing

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 
 def element_at(my_list, idx):
-    if len(my_list) - 1 < idx < 0:
-        return None
-    return my_list[idx]
+    if 0 <= idx <= len(my_list)-1:
+        return my_list[idx]
+    return None
