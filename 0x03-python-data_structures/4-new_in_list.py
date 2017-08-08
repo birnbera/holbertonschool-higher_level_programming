66efaeb4224b3c45e10bdb9c93e0615612410065
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-08 19:59:27 +0000

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    new_list = [i for i in my_list]
+    if idx > len(new_list) - 1:
+        return new_list
+    new_list[idx] = element
+    return new_list
