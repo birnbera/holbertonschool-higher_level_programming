eea9ce7b1978984065f0eb34e87f23da41948f99
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-10 21:05:21 +0000

    Check for attribute items

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 
 
 def best_score(my_dict):
-    if my_dict is not None:
+    if my_dict is not None and hasattr(my_dict, "items"):
         return max(my_dict.items(), default=None, key=lambda x: x[1])[0]
     return None
