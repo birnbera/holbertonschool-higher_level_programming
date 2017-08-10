50c687edcea0cbc53d114c00eb2f60c71179a56f
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-10 21:08:46 +0000

    Return None for empty list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 
 
 def best_score(my_dict):
-    if my_dict is not None:
-        return max(my_dict.items(), default=None, key=lambda x: x[1])[0]
+    if my_dict:
+        return max(my_dict.items(), key=lambda x: x[1])[0]
     return None
