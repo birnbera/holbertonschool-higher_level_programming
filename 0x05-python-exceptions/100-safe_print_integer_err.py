0e6f12c3e6068b29efcda38517aecf4ebcd2054f
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-22 04:01:42 +0000

    Remove else clause

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,7 @@ def safe_print_integer_err(value):
     ret = False
     try:
         print("{:d}".format(value))
+        ret = True
     except (TypeError, ValueError):
         message = "Unknown format code 'd' " \
         "for object of type '{:s}'".format(value.__class__.__name__)
@@ -14,7 +15,5 @@ def safe_print_integer_err(value):
                            None,
                            limit=0,
                            chain=False)
-    else:
-        ret = True
     finally:
         return ret
