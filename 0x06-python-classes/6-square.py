7c245312e8ea48f1d2653c19ec40b722bc85e61a
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-22 23:13:26 +0000

    Test for integer > 0 for position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,7 +40,7 @@ class Square:
     def position(self, value):
         if type(value) is not tuple or \
            len(value) is not 2 or \
-           any(map(lambda x: x < 0, value)):
+           any(map(lambda x: type(x) is not int or x < 0, value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
