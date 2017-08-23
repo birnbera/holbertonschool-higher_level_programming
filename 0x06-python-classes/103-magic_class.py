c1d3f8a0236a358c51ea002ced79c3ee680f6464
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-23 05:43:09 +0000

    Make radius private variable

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,18 +7,18 @@ class MagicClass:
         """Initialize circle with default radius=0
 
         __init__ method initializes radius and test if numeric"""
-        self._MagicClass__radius = 0
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """Calculate area of circle based on radius"""
-        return self._MagicClass__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """Calculate circumference with given radius"""
-        return 2 * math.pi * self._MagicClass__radius
+        return 2 * math.pi * self.__radius
 
 import dis
 
