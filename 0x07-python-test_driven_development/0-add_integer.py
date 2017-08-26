33e69922ee323657b4786fc02801f34a06a52648
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-26 18:06:35 +0000

    Handle inf and nan

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -27,4 +27,7 @@ def add_integer(a, b):
         raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
+    result = a + b
+    if result != result or result == float('inf') or result == -float('inf'):
+       raise OverflowError
     return int(a) + int(b)
