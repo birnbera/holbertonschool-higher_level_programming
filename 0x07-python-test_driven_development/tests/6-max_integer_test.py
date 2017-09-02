99c16f6e02f9777ce1cf3fe1346c80af5c9779ae
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-09-02 03:51:21 +0000

    Cover more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -18,11 +18,25 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
+    def test_one_element(self):
+        self.assertEqual(max_integer([2]), 2)
+
+    def test_negative(self):
+        self.assertEqual(max_integer(list(range(-10, 0))), -1)
+        self.assertEqual(max_integer([-1, 10, 3, 2, 1, 99]), 99)
+
     def test_max_sorted(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_max_desorted(self):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_max_at_start(self):
+        self.assertEqual(max_integer(list(range(10, 0, -1))), 10)
+
+    def test_all_equal(self):
+        self.assertEqual(max_integer([10] * 10), 10)
+        self.assertEqual(max_integer([-1] * 10), -1)
 
     def test_wrong_type(self):
         self.assertRaises(TypeError, max_integer, 1234)
@@ -33,3 +47,8 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_floats(self):
         self.assertEqual(max_integer([1.1, 4.5, 500.2, 300.0]), 500.2)
+
+    def test_inf_nan(self):
+        self.assertEqual(max_integer([5, 10, float('inf')]), float('inf'))
+        self.assertEqual(max_integer([5, 10, -float('inf')]), 10)
+        self.assertEqual(max_integer([1, 2, 3, float('nan')]), 3)
