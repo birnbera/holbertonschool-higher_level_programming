b282368d0dfb43a96e1a83902fc7fa345fd84623
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-09-12 22:50:45 +0000

    Update formatting of test scripts

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -83,48 +83,53 @@ class TestSquare(unittest.TestCase):
             sys.stdout.seek(0)
             sys.stdout.truncate(0)
 
-        def test_size(self):
-            s1 = Square(5)
-            self.assertEqual(s1.size, 5)
-            self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 5")
-            s1.size = 10
-            self.assertEqual(s1.size, 10)
-            self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 10")
-            with self.assertRaise(TypeError, "width must be an integer"):
-                s1.size = "9"
+    def test_size(self):
+        """Test that Square `size` attribute is set correctly"""
+        Square._Base__nb_object = 0
+        s1 = Square(5)
+        self.assertEqual(s1.size, 5)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 5")
+        s1.size = 10
+        self.assertEqual(s1.size, 10)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 10")
+        with self.assertRaises(TypeError, msg="width must be an integer"):
+            s1.size = "9"
 
-        def test_update_args_kwargs(self):
-            s1 = Square(5)
-            self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 5")
-            s1.update(10)
-            self.assertEqual(s1.__str__(), "[Square] (10) 0/0 - 5")
-            s1.update(1, 2)
-            self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 2")
-            s1.update(1, 2, 3)
-            self.assertEqual(s1.__str__(), "[Square] (1) 3/0 - 2")
-            s1.update(1, 2, 3, 4)
-            self.assertEqual(s1.__str__(), "[Square] (1) 3/4 - 2")
-            s1.update(x=12)
-            self.assertEqual(s1.__str__(), "[Square] (1) 12/4 - 2")
-            s1.update(size=7, y=1)
-            self.assertEqual(s1.__str__(), "[Square] (1) 12/1 - 7")
-            s1.update(size=7, id=89, y=1)
-            self.assertEqual(s1.__str__(), "[Square] (89) 12/1 - 7")
+    def test_update_args_kwargs(self):
+        """Test that `update()` method correctly handles *args and **kwargs"""
+        Square._Base__nb_object = 0
+        s1 = Square(5)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 5")
+        s1.update(10)
+        self.assertEqual(s1.__str__(), "[Square] (10) 0/0 - 5")
+        s1.update(1, 2)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 2")
+        s1.update(1, 2, 3)
+        self.assertEqual(s1.__str__(), "[Square] (1) 3/0 - 2")
+        s1.update(1, 2, 3, 4)
+        self.assertEqual(s1.__str__(), "[Square] (1) 3/4 - 2")
+        s1.update(x=12)
+        self.assertEqual(s1.__str__(), "[Square] (1) 12/4 - 2")
+        s1.update(size=7, y=1)
+        self.assertEqual(s1.__str__(), "[Square] (1) 12/1 - 7")
+        s1.update(size=7, id=89, y=1)
+        self.assertEqual(s1.__str__(), "[Square] (89) 12/1 - 7")
 
-        def test_to_dict(self):
-            """Test that `to_dictionary()` method produces valid dictionary
-            representation of Square instance. Converts to dictionary and
-            updates distinct instance to those values and compares the two
-            resulting objects to show that they have the same attributes but
-            are not identical objects.
-            """
-            s1 = Square(10, 2, 1)
-            self.assertEqual(s1.__str__(), "[Square] (1) 2/1 - 10")
-            self.assertEqual(s1.to_dictionary(), {'id': 1, 'x': 2,
-                                                  'size': 10, 'y': 1})
-            self.assertIs(type(s1.to_dictionary()), dict)
-            s2 = Square(1, 1)
-            self.assertEqual(s2.__str__(), "[Square] (2) 1/0 - 1")
-            s2.update(**s1.to_dictionary())
-            self.assertEqual(s2.__str__(), "[Square] (1) 2/1 - 10")
-            self.assertNotEqual(s1, s2)
+    def test_to_dict(self):
+        """Test that `to_dictionary()` method produces valid dictionary
+        representation of Square instance. Converts to dictionary and
+        updates distinct instance to those values and compares the two
+        resulting objects to show that they have the same attributes but
+        are not identical objects.
+        """
+        Square._Base__nb_object = 0
+        s1 = Square(10, 2, 1)
+        self.assertEqual(s1.__str__(), "[Square] (1) 2/1 - 10")
+        self.assertEqual(s1.to_dictionary(), {'id': 1, 'x': 2,
+                                              'size': 10, 'y': 1})
+        self.assertIs(type(s1.to_dictionary()), dict)
+        s2 = Square(1, 1)
+        self.assertEqual(s2.__str__(), "[Square] (2) 1/0 - 1")
+        s2.update(**s1.to_dictionary())
+        self.assertEqual(s2.__str__(), "[Square] (1) 2/1 - 10")
+        self.assertNotEqual(s1, s2)
