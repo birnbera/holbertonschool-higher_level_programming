2d928fbe3b77440e01054ecde2b9730502fe331e
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-17 00:08:01 +0000

    101-avg_temperatures.sql use uppercase

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- Load data from temperatures.sql into specified database
 -- and display average temperature by city ordered by temperature.
-source temperatures.sql
+SOURCE temperatures.sql
 SELECT `city`, AVG(`value`) AS `avg_temp`
        FROM temperatures
        GROUP BY `city`
