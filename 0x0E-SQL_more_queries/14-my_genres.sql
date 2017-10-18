1dbc73c70497d1151710e5cec3547a94d5cf14f3
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-18 01:00:16 +0000

    Order by genre ascending

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@ SELECT tv_genres.name AS name
        ON tv_genres.id = tv_show_genres.genre_id
        INNER JOIN tv_shows
        ON tv_shows.id = tv_show_genres.show_id
-       WHERE tv_shows.title = "Dexter";
+       WHERE tv_shows.title = "Dexter"
+       ORDER BY name ASC;
