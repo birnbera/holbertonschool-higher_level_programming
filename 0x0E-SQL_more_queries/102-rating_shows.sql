3db76c558c47d725110afc552b3ad953f60b5342
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-18 02:30:02 +0000

    Use alias for column instead of full expression

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -5,4 +5,4 @@ SELECT tv_shows.title AS title,
        INNER JOIN tv_show_ratings
        ON tv_shows.id = tv_show_ratings.show_id
  GROUP BY tv_shows.id
- ORDER BY sum(tv_show_ratings.rate) DESC;
+ ORDER BY rating DESC;
