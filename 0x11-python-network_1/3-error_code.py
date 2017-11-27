4b9429ae8d6e013fdd906e81716cbb93bccc40d1
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-11-27 23:53:35 +0000

    if name is main...

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,14 @@
 """Script that sends URL request and prints status code on error,
 or the body on success."""
 
-import urllib.error
-import urllib.request
-import sys
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as r:
-        print(r.read().decode('utf8'))
-except urllib.error.HTTPError as e:
-    print('Error code: {}'.format(e.code))
+if __name__ == "__main__":
+    import urllib.error
+    import urllib.request
+    import sys
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as r:
+            print(r.read().decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
