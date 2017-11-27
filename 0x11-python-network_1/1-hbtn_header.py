d5d427025a57b83ea8208c472dcc8cf45041a8b9
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-11-27 23:28:21 +0000

    Revert "Remove error handling"
    This reverts commit f0696909a6f1b5a9e6e73ccc44301fff0353b01c.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,11 @@
 """Script to make a request to a url passed on the command line"""
 
 import urllib.request
+import urllib.error
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as r:
-    print(r.getheader('X-Request-Id', ''))
+try:
+    with urllib.request.urlopen(sys.argv[1]) as r:
+        print(r.getheader('X-Request-Id', ''))
+except (urllib.error.URLError, IndexError) as e:
+    print(e)
