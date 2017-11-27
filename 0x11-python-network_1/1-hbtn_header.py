1bcc89c6dc04e76584ed253f7f2935f5b4f74d82
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-11-27 23:50:08 +0000

    Revert "not absolute import"
    This reverts commit e72949c348a166d17400fb0ff507acf3fe53f8f4.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,12 +2,12 @@
 """Script to make a request to a url passed on the command line"""
 
 if __name__ == "__main__":
-    from urllib.request import urlopen
-    from urllib.error import URLError
-    from sys import argv
+    import urllib.request
+    import urllib.error
+    import sys
 
     try:
-        with urlopen(argv[1]) as r:
+        with urllib.request.urlopen(sys.argv[1]) as r:
             print(r.getheader('X-Request-Id', ''))
-    except (URLError, IndexError) as e:
+    except (urllib.error.URLError, IndexError) as e:
         print(e)
