2258feb4f34c072ce388e1df83b4c72b3fc70ce2
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-11-28 19:13:16 +0000

    Reverse order of parameters

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     from requests import get
     from sys import argv
 
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
 
     try:
         r = get(url)
         j = r.json()
         for commit in j[:10]:
             print('{}: {}'.format(commit.get('sha'),
-                                  commit.get('commit')
-                                  .get('author')
-                                  .get('name')))
-    except Exception as e:
+                                       commit.get('commit')
+                                       .get('author')
+                                       .get('name')))
+    except IndexError as e:
         print(e)
