d9698348055f5d24ce62d42c57d133f8323a9096
Andrew Birnberg <andrew.birnberg@gmail.com>
2018-01-10 20:29:07 +0000

    Fix variable name typo

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs  = require('fs');
+const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) console.log(err);
-  else console.log(body);
+  else console.log(data);
 });
