43f6427f538a87059237755d6464eaf4648b50a8
Andrew Birnberg <andrew.birnberg@gmail.com>
2018-01-10 00:14:40 +0000

    Use require to extend old square version

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,33 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class OldSquare extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
-
+const OldSquare = require('./5-square')
 module.exports = class Square extends OldSquare {
   charPrint (c) {
     if (c == undefined) c = 'X';
